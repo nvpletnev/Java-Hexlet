c747d93c989dfd28e8fa397496446e48911c8842
nvpletnev <nvpletnev@gmail.com>
2018-09-03 20:13:28 +0300

    first commit

---
+++ src/io/hexlet/ds/ListImpl.java
@@ -0,0 +1,10 @@
+package io.hexlet.ds;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class ListImpl<T> implements List<T> {
+
+}
